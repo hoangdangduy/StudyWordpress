0dfbbfc970f28f65d7468e6e8f28a2fa6fe62aa9
HoangDD <dangduyhoang3009@gmail.com>
2018-09-14 06:32:23 +0700

    Bai 4

--- functions.php
+++ functions.php
@@ -1,3 +1,4 @@
+<?php
 /**
   @ Thiết lập các hằng dữ liệu quan trọng
   @ THEME_URL = get_stylesheet_directory() - đường dẫn tới thư mục theme
@@ -26,7 +27,7 @@
        function thachpham_theme_setup() {
 
        }
-       add_action( 'init', $function_to_add, $priority = 10, $accepted_args = 1 )
+       add_action( 'init', 'thachpham_theme_setup' );
   }
 
 /*
@@ -39,3 +40,85 @@ load_theme_textdomain( 'thachpham', $language_folder );
 * Tự chèn RSS Feed links trong <head>
 */
 add_theme_support( 'automatic_feed_links' );
+
+/*
+* Thêm chức năng post thumbnail
+*/
+add_theme_support( 'post-thumbnail' );
+
+/*
+* Thêm chức năng title-tag để tự thêm <title>
+*/
+add_theme_support( 'title-tag' );
+
+/*
+* Thêm chức năng post format
+*/
+add_theme_support( 'post-formats', array(' image ', ' video ', 'gallery ', 'quote', 'link') );
+
+/*
+* Thêm chức năng custom background
+*/
+$default_background = array ( 'default-color ' => '#e8e8e8' );
+add_theme_support( 'custom-background', $default_background);
+
+/*
+* Tạo menu cho theme
+*/
+register_nav_menu( 'primary-menu', __('Primary Menu', 'thachpham'));
+
+/*
+* Tạo sidebar cho theme
+*/
+$sidebar = array(
+    'name' => __( 'Main sidebar', 'thachpham' ),
+    'id' => 'main-sidebar',
+    'description' => 'Main sidebar for Thachpham theme',
+    'class' => 'main-sidebar',
+    'before_title' => '<h3 class="widgettitle">',
+    'after_title' => '</h3>'
+);
+
+/**
+@ Thiết lập hàm hiển thị logo
+@ thachpham_logo()
+**/
+if ( !function_exists( 'thachpham_logo' )) {
+    function thachpham_logo() { ?>
+        <div class="logo">
+            <div class="site-name">
+                <?php if (is_home()) {
+                    printf('<h1><a href="%1$s" title="%2$s">%3$s</a></h1>',
+                        get_bloginfo( 'url' ),
+                        get_bloginfo( 'description' ),
+                        get_bloginfo( 'sitename')
+                    );
+                } else {
+                    printf(
+                        '<p><a href="%1$s" title="%2$s">%3$s</a></p>',
+                        get_bloginfo( 'url' ),
+                        get_bloginfo( 'description' ),
+                        get_bloginfo( 'sitename' )
+                    );
+                } // endif ?>
+            </div>
+            <div class="site-description"><?php  bloginfo( 'description' );?></div>
+        <?php>
+    }
+}
+
+/**
+@   Thiết lập hàm hiển thị menu
+@   thachpham_menu( $slug )
+**/
+// if ( !function_exists( 'thachpham_menu' )) {
+//     function thachpham_menu( $slug ) {
+//         $menu = array(
+//             'theme_location' => $slug,
+//             'container' => 'nav',
+//             'container_class' => $slug,
+//         );
+//         wp_nav_menu( $menu );
+//     }
+// }
+?>
