9b8b3945e9104f0a87996a766eb2201c8ef93877
HoangDD <dangduyhoang3009@gmail.com>
2018-09-26 00:16:54 +0700

    https://thachpham.com/wordpress/wordpress-development/lap-trinh-theme-wordpress-code-cho-content-php.html

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
         <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
                 <?php get_template_part( 'content', get_post_format() ); ?>
         <?php endwhile; ?>
+        <?php thachpham_pagination(); ?>
         <?php else : ?>
                 <?php get_template_part( 'content', 'none' ); ?>
         <?php endif; ?>
